fb1dbd5409ed4d6eb1b1ef7e01af49a62f88380d
Agastya Mondal <amondal13@gmail.com>
2018-10-20 13:20:31 -0400

    add pytesseract parsing

--- server/app.py
+++ server/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, flash, request, redirect, url_for
 from flask_cors import CORS, cross_origin
+from PIL import Image
+import pytesseract
+import os
 app = Flask(__name__)
 cors = CORS(app)
+UPLOAD_FOLDER = '.'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def parse():
-    print(request.files)
-    return 'Hello, World!'
+    file = request.files['file']
+    filename = file.filename
+    dir_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(dir_path)
+    text = pytesseract.image_to_string(Image.open(dir_path))
+    return text
